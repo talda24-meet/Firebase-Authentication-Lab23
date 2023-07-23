01bedb719acf092717621b65d9b6856113151efb
talda24-meet <139680993+talda24-meet@users.noreply.github.com>
2023-07-23 12:13:25 +0300

    unfinished

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,6 +2,20 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+const firebaseConfig = {
+    "apiKey": "AIzaSyDEyKgkZwD9gOORsunsw8Sl0wB4Dr7v7Ro",
+    "authDomain": "taltest-a00ba.firebaseapp.com",
+    "projectId": "taltest-a00ba",
+    "storageBucket": "taltest-a00ba.appspot.com",
+    "messagingSenderId": "453121566224",
+    "appId": "1:453121566224:web:b5bf83f6900995b7048c95",
+    "measurementId": "G-4QSQ8F5582"
+    "databaseURL": ""
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
@@ -13,6 +27,15 @@ def signin():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        
+    try:
+        login_session['user'] = auth.create_user_with_email_and_password(email, password)
+        return redirect(url_for('add_tweet.html'))
+    except:
+        error = "Authentication failed"
     return render_template("signup.html")
 
 
